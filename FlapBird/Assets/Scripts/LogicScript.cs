1e9f653f477c6f7a1db64fb7657ccd526d2b04f0
MohammadRashidEhb <mohammad.rashid@student.ehb.be>
2024-10-28 01:18:07 +0100

    Feat: game-mechanics: Game Over & Restart Game functionality added
    In this phase, I added a game over and restart game functionality

--- FlapBird/Assets/Scripts/LogicScript.cs
+++ FlapBird/Assets/Scripts/LogicScript.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 // This scripts handles the game logic related to the player's score 
 public class LogicScript : MonoBehaviour
 {
     public int playerScore; // Variable to store score
     public Text scoreText; // Ref to the UI text component that displays the score
+    public GameObject gameOverScreen;
 
     // Call from the Unity editor to increase the score.
     [ContextMenu("Increase Score")]
@@ -15,5 +17,15 @@ public class LogicScript : MonoBehaviour
     {
         playerScore = playerScore +scoreToAdd;
         scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver() 
+    {
+        gameOverScreen.SetActive(true);
     }
 }
